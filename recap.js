a5f4bd9cb3f66999042dfc2d1f3fab07106d8500
linus kiogora <linuskamau586@gmail.com>
2023-05-02 10:41:47 +0300

    js recap

--- recap.js
+++ recap.js
@@ -8,4 +8,23 @@ function displayNames( ) {
   let element = document.getElementById("myElement");
   element.innerHTML = updatedNames.join(", ");
 }
-console.log("hey");
+
+//asnyc study
+const event=new Promise((resolve,reject)=>{
+  var name= "john";
+  if (name=="pedro"){
+    resolve( name)
+  }else
+  {
+    reject("Name is not pedro the Name is " + name )
+  }
+});
+event.then( 
+  ( name)=>{ console.log(name)
+  }
+)
+.catch( 
+  (err )=>{
+    console.log( err)
+  }
+)
